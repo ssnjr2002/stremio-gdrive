b596d3c3d3868efc72b0f0de1ebf16d32eb1c5c5
ssnjrthegr8 <ledarkknight18@gmail.com>
2021-09-30 17:26:54 +0530

    Definitive fix for passing invalid file names

--- cf_proxy.js
+++ cf_proxy.js
@@ -7,14 +7,15 @@ var credentials = {
 async function handleRequest(request) {
   const drive = new gdrive(credentials)
   let url = new URL(request.url)
-  let path = url.pathname
-  if(path.startsWith("/load")) {
-    var file_id = url.searchParams.get('i')
-    var file_name = url.searchParams.get('n')
+  let path_split = url.pathname.split('/')
+  if (path_split[1] == 'load') {
+    var file_id = path_split[2]
+    var file_name = path_split[3] || 'file_name.vid'
     return drive.streamFile(request.headers.get("Range"), file_id, file_name)
-  } 
-  else {
-    return new Response(':)', {"status": 200})
+  } else if (!path_split[2]) {
+    return new Response('404: File Not Found!', { "status": 404 })
+  } else {
+    return new Response('200', { "status": 200 })
   }
 }
 
@@ -25,39 +26,23 @@ class gdrive {
   }
   async streamFile(range = "", file_id, file_name) {
     //console.log(`streamFile: ${file_id}, range: ${range}`)
-
     let fetchURL = `${this.gapihost}/drive/v3/files/${file_id}?alt=media`
     let fetchData = await this.authData()
     fetchData.headers['Range'] = range
-
     let streamResp = await fetch(fetchURL, fetchData)
     let { readable, writable } = new TransformStream()
     streamResp.body.pipeTo(writable)
-
-    let { headers } = (streamResp = new Response(readable, streamResp))
-    let tokenExpiryDate = this.credentials.token.expires_in
-    let maxAge = Math.floor((tokenExpiryDate - Date.now()) / 1000)
-
-    //console.log(`expiryDate: ${tokenExpiryDate}/${Date()} maxAge: ${maxAge}`)
-
-    headers.set('Date', Date())
-    headers.set('Expires', new Date(tokenExpiryDate))
-    headers.set('Cache-Control', `max-age=${maxAge}`)
-    headers.set('Access-Control-Allow-Origin', '*')
-    headers.set('Access-Control-Allow-Headers', '*')
-    headers.set('Content-Disposition', `attachment; filename = "${file_name}"`)
-    
-    return streamResp
+    return new Response(readable, streamResp)
   }
   async accessToken() {
     //console.log("accessToken")
-    if(!this.credentials.token || this.credentials.token.expires_in < Date.now()) {
+    if (!this.credentials.token || this.credentials.token.expires_in < Date.now()) {
       this.credentials.token = await this.fetchAccessToken()
       this.credentials.token.expires_in = Date.now() + this.credentials.token.expires_in * 1000
     }
     return this.credentials.token.access_token
   }
-  async fetchAccessToken(url=`${this.gapihost}/oauth2/v4/token`) {
+  async fetchAccessToken(url = `${this.gapihost}/oauth2/v4/token`) {
     //console.log("fetchAccessToken")
     let jsonBody = {
       'client_id': this.credentials.client_id,
@@ -65,12 +50,12 @@ class gdrive {
       'refresh_token': this.credentials.refresh_token,
       'grant_type': 'refresh_token'
     }
-    let response = await fetch(url, {method: 'POST',body: JSON.stringify(jsonBody)})
+    let response = await fetch(url, { method: 'POST', body: JSON.stringify(jsonBody) })
     return await response.json()
   }
-  async authData(headers={}) {
+  async authData(headers = {}) {
     headers['Authorization'] = `Bearer ${await this.accessToken()}`;
-    return {'method': 'GET', 'headers': headers}
+    return { 'method': 'GET', 'headers': headers }
   }
 }
 
